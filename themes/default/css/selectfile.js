b1b6940ce2d39dea9c0e9cade7475db1c52c52eb
Kat Chuang <katychuang@gmail.com>
2014-04-13 16:06:02 -0400

    edit js path

--- themes/default/css/selectfile.js
+++ themes/default/css/selectfile.js
@@ -1,1 +1,1 @@
-$(function (){$('#themes').change(function (){changeTheme($(':selected').val())})});function changeTheme(theme){var stylshit = $('[title="hakyll_theme"]');stylshit.attr('href','/css/'+theme.toLowerCase()+'.css')}
+$(function (){$('#themes').change(function (){changeTheme($(':selected').val())})});function changeTheme(theme){var stylshit = $('[title="hakyll_theme"]');stylshit.attr('href','./css/'+theme.toLowerCase()+'.css')}
