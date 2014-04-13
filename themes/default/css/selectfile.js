0be594dde978e067c71a50944ae547e85620d456
Kat Chuang <katychuang@gmail.com>
2014-04-13 15:57:44 -0400

    files for making preview possible

---
+++ themes/default/css/selectfile.js
@@ -0,0 +1,1 @@
+$(function (){$('#themes').change(function (){changeTheme($(':selected').val())})});function changeTheme(theme){var stylshit = $('[title="hakyll_theme"]');stylshit.attr('href','/css/'+theme.toLowerCase()+'.css')}
