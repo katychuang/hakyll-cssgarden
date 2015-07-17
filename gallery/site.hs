5947dc04028a8ba78e01c407e6dee81d942758a4
katychuang <katychuang@gmail.com>
2015-07-17 00:25:26 -0400

    page headers and such updated

--- gallery/site.hs
+++ gallery/site.hs
@@ -66,7 +66,7 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    constField "title" "Recent Themes"      `mappend`
                     defaultContext
 
             getResourceBody
