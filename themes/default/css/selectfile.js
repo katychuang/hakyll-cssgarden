89fc3681f34fa25802a8e140499bf3643392bb85
katychuang <katychuang@gmail.com>
2017-12-13 21:44:12 -0500

    pretty format js file

--- themes/default/css/selectfile.js
+++ themes/default/css/selectfile.js
@@ -1,1 +1,34 @@
-$(function (){$('#themes').change(function (){var item = $(':selected').val();changeTheme(item);$('.styleInfo').html("Currently viewing <a href='/css/" + item + ".css'>" + item + "</a>")})});function changeTheme(theme){var stylshit = $('[title="hakyll_theme"]');stylshit.attr('href','./css/'+theme.toLowerCase()+'.css')}
+$(function() {
+    var query = location.search.substr(1);
+    var s = "";
+    var tValue = "default";
+    query.split("&").forEach(function(part) {
+        var item = part.split("=");
+        s += " " + item;
+        if (item[0] == "theme") {
+            tValue = item[1]
+        }
+    });
+    changeTheme(tValue);
+    updateLinks(tValue);
+    if (window.location.href.indexOf("posts") >= 0) {
+        $('[title="hakyll_theme"]').attr('href', '../css/' + tValue.toLowerCase() + '.css')
+    } else {
+        $('.styleInfo').html("Currently viewing <a href='./css/" + tValue + ".css' style='background:yellow;color:black;padding:4px;border-radius:4px'>" + tValue + "</a> ")
+    }
+    $('#themes').change(function() {
+        var item = $(':selected').val();
+        window.location = location.search.split('?')[0] + '?theme=' + item
+    })
+});
+
+function changeTheme(theme) {
+    var stylshit = $('[title="hakyll_theme"]');
+    stylshit.attr('href', './css/' + theme.toLowerCase() + '.css')
+}
+
+function updateLinks(theme) {
+    $.each($('a'), function(index, value) {
+        $(value).attr('href', $(value).attr('href') + '?theme=' + theme)
+    })
+}
